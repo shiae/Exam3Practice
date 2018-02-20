e1a5d3aff803c4d2069f0cec49ba6929bca79e2e
Allison Shi <shiae@rose-hulman.edu>
2018-02-19 21:00:24 -0500

    m2 understood

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -5,7 +5,7 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS, SEQUENCES and MUTATION  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Allison Shi.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
--- src/m2_mutating_returning_lists.py
+++ src/m2_mutating_returning_lists.py
@@ -111,6 +111,13 @@ def doubler(list1, list2):
     #    TIME ESTIMATE:   5 minutes.
     # ------------------------------------------------------------------
 
+    for k in range(len(list1)):
+        list1[k] = list1[k] * 2
+
+    new_list = []
+    for k in range(len(list2)):
+        new_list = new_list + [list2[k] * 2]
+    return new_list
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
